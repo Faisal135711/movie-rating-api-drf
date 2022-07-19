5bf6e41cf3068b71b1e7922dd54d550d97d0d771
Faisal135711 <alpha316mnr@gmail.com>
2022-07-19 22:53:41 +0600

    feat: implement serializers get request

---
+++ watchlist_app/api/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from watchlist_app.api.views import (
+    movie_list,
+    movie_details,
+)
+
+urlpatterns = [
+    path('list/', movie_list, name='movie-list'),
+    path('<int:pk>/', movie_details, name='movie-detail'),
+]
---
+++ watchlist_app/api/views.py
@@ -0,0 +1,17 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from watchlist_app.models import Movie
+from watchlist_app.api.serializers import MovieSerializer
+
+@api_view(['GET'])
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True) 
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def movie_details(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    serializer = MovieSerializer(movie) 
+    return Response(serializer.data)
