1e9e5fefa5843c5fdc4397283e1990577b690312
Faisal135711 <alpha316mnr@gmail.com>
2022-07-21 22:33:54 +0600

    feat: create custom serializer field name_len fo MovieSerializer

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -3,10 +3,15 @@ from rest_framework import serializers
 from watchlist_app.models import Movie, Drama
 
 class MovieSerializer(serializers.ModelSerializer):
+    name_len = serializers.SerializerMethodField()
+
     class Meta:
         model = Movie
         fields = "__all__"
         # exclude = ['name']
+
+    def get_name_len(self, object):
+        return len(object.name)
         
         
 class DramaSerializer(serializers.Serializer):
