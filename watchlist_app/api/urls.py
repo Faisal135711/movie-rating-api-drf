350309df61564223c148a53b4958a6f7bc671030
Faisal135711 <alpha316mnr@gmail.com>
2022-07-21 10:13:03 +0600

    feat: implement movie crud using APIView

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -2,18 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 
 from watchlist_app.api.views import (
-    movie_list,
-    movie_details,
-    drama_list,
-    drama_details
+    MovieListAV,
+    MovieDetailAV,
 )
 
 urlpatterns = [
-    path('list/', movie_list, name='movie-list'),
-    # path('<int:pk>/', movie_details, name='movie-detail'),
+    path('list/', MovieListAV.as_view(), name='movie-list'),
+    path('<int:pk>/', MovieDetailAV.as_view(), name='movie-detail'),
 
 
     # practice
-    path('', drama_list, name='drama-list'),
-    path('<int:pk>/', drama_details, name='drama-detail'),
+    # path('', drama_list, name='drama-list'),
+    # path('<int:pk>/', drama_details, name='drama-detail'),
 ]
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,91 +1,52 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.views import APIView
 
 from watchlist_app.models import Movie, Drama
 from watchlist_app.api.serializers import MovieSerializer, DramaSerializer
 
-@api_view(['GET', 'POST'])
-def movie_list(request):
-    if request.method == 'GET':
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True) 
+
+class MovieListAV(APIView):
+    def get(self, request):
+        movies = Movie.objects.all()  
+        serializer = MovieSerializer(movies, many=True)      
         return Response(serializer.data)
     
-    if request.method == 'POST':
+    def post(self, request):
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        else: 
+        else:
             return Response(serializer.errors)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def movie_details(request, pk):
-    if request.method == 'GET':
+
+class MovieDetailAV(APIView):
+    def get(self, request, pk):
         try:
             movie = Movie.objects.get(pk=pk)
-        except Movie.DoesNotExist:
-            return Response({'error': 'Movie not found' },  status=status.HTTP_404_NOT_FOUND)
-        serializer = MovieSerializer(movie) 
+        except:
+            return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = MovieSerializer(movie)
         return Response(serializer.data)
     
-    if request.method == 'PUT':
+    def put(self, request, pk):
         try:
             movie = Movie.objects.get(pk=pk)
-        except Movie.DoesNotExist:
-            return Response({'error': 'Movie not found' },  status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
         serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
-        
-    if request.method == 'DELETE':
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
         try:
             movie = Movie.objects.get(pk=pk)
-        except Movie.DoesNotExist:
-            return Response({'error': 'Movie not found' },  status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET', 'POST'])
-def drama_list(request):
-    if request.method == 'GET':
-        dramas = Drama.objects.all()
-        serializer = DramaSerializer(dramas, many=True)
-        return Response(serializer.data)
-
-    if request.method == 'POST':
-        serializer = DramaSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def drama_details(request, pk):
-    try:
-        drama = Drama.objects.get(pk=pk)
-    except:
-        return Response({'error': 'Drama not found' },  status=status.HTTP_404_NOT_FOUND) 
-
-    if request.method == 'GET':
-        serializer = DramaSerializer(drama)
-        return Response(serializer.data)
-
-    if request.method == 'PUT':
-        serializer = DramaSerializer(drama, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-
-    if request.method == 'DELETE':
-        drama.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-        
