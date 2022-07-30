5ccedd71a35d043cf90356dc04d72f4dfb709836
Faisal135711 <alpha316mnr@gmail.com>
2022-07-30 11:55:25 +0600

    feat: apply concrete view classes

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -24,26 +24,15 @@ from watchlist_app.api.serializers import (
 )
 
 
-class ReviewList(mixins.ListModelMixin, 
-                 mixins.CreateModelMixin,
-                 generics.GenericAPIView):
+class ReviewList(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
+    
 
 class WatchListAV(APIView):
     def get(self, request):
