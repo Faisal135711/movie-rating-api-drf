8e540b73d0831565e9b0355a9a0214f85d6891fb
Faisal135711 <alpha316mnr@gmail.com>
2022-08-06 16:48:40 +0600

    feat: implement basic authentication

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -130,3 +130,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #         'rest_framework.permissions.IsAuthenticated',
 #     ]
 # }
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ]
+}
