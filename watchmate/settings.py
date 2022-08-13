4ea738084103d58b0265995a42347d5bb729980d
Faisal135711 <alpha316mnr@gmail.com>
2022-08-13 18:26:33 +0600

    feat: disable browsable api

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -151,4 +151,8 @@ REST_FRAMEWORK = {
 
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     # 'PAGE_SIZE': 2,
+
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
