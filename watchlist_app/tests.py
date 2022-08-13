a26f30a127023b838c7c776f52ed5daf1576ed53
Faisal135711 <alpha316mnr@gmail.com>
2022-08-13 21:48:51 +0600

    feat: write testcase for watchlist

--- watchlist_app/tests.py
+++ watchlist_app/tests.py
@@ -35,3 +35,39 @@ class StreamPlatformTestCase(APITestCase):
     def test_streamplatform_ind(self):
         response = self.client.get(reverse('streamplatform-detail', args=(self.stream.id,)))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class WatchListTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="12345")
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects.create(name="Netflix",
+                                                           about="netflix",
+                                                           website="https://netflix.com")
+
+        self.watchlist = models.WatchList.objects.create(platform=self.stream, 
+                                                         title="Py vs Js",
+                                                         description="py vs js",
+                                                         active=True,
+                                                        )
+    
+    def test_watchlist_create(self):
+        data = {
+            "platform": self.stream,
+            "title": "Py vs Js",
+            "description": "python vs javascript",
+            "active": True
+        }
+        response = self.client.post(reverse("movie-list"), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_watchlist_list(self):
+        response = self.client.get(reverse('movie-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_watchlist_ind(self):
+        response = self.client.get(reverse('movie-detail', args=(self.watchlist.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.WatchList.objects.get().title, "Py vs Js")
