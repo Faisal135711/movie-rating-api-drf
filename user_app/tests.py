12b8c7868732083c3ce08c12aaf20a8e37ee46f4
Faisal135711 <alpha316mnr@gmail.com>
2022-08-13 19:36:02 +0600

    feat: write test for user registration

--- user_app/tests.py
+++ user_app/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status 
+from rest_framework.authtoken.models import Token 
 
-# Create your tests here.
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class RegisterTestCase(APITestCase):
+    def test_register(self):
+        data = {
+            "username": "testcase",
+            "email": "test@test.com",
+            "password": "123321",
+            "password2": "123321"
+        }
+
+        response = self.client.post(reverse("register"), data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
